71d2aecb203c7f3d1ef1e4035bd08ef4944c3f8b
mbh00 <newgoku00@gmail.com>
2024-01-08 18:11:31 +0100

    q

--- app/Filament/Resources/QuestionResource.php
+++ app/Filament/Resources/QuestionResource.php
@@ -58,7 +58,8 @@ class QuestionResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('text')->limit(100)->html()
                 ->searchable()->sortable(),
-                Tables\Columns\TextColumn::make('moduleRel.name')->label('Modules')->sortable(),
+                Tables\Columns\TextColumn::make('moduleRel.name')->label('Module')->sortable(),
+                Tables\Columns\TextColumn::make('certif.name')->label('Certification')->sortable(),
                 Tables\Columns\TextColumn::make('answers_count')->counts('answers')->label('Answers')
                 ->numeric()->sortable(),
                         //     Tables\Columns\IconColumn::make('isexam')
