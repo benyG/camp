ce60516259c839b3d05ef2c58c0dd4185829522b
mbh00 <newgoku00@gmail.com>
2024-02-27 09:00:03 +0100

    uuu

---
+++ app/Filament/Widgets/UserOverview.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+use App\Models\Exam;
+use App\Models\Module;
+use App\Models\Question;
+use App\Models\Course;
+use App\Models\User;
+use App\Models\Vague;
+
+class UserOverview extends Widget
+{
+    protected static ?int $sort = -1;
+    protected static bool $isLazy = true;
+    protected static string $view = 'filament.widgets.us-ovv';
+   // protected int | string | array $columnSpan = 'full';
+
+    #[Locked]
+    public $va;
+    #[Locked]
+    public $co;
+    #[Locked]
+    public $mo;
+    #[Locked]
+    public $qu;
+    #[Locked]
+    public $iac;
+
+    public function mount(){
+        $ix=cache()->rememberForever('settings', function () {
+            return \App\Models\Info::findOrFail(1);
+        });
+      //  $uarr=User::where('ex','<>',0)->where('id','<>',auth()->user()->id)->with('exams2')->get();
+        $earr=Exam::has('users1')->with('users1')->get();
+        $eall=$earr->pluck('id');
+        $rt=Question::with('answers')->get();
+        $nt=auth()->user()->exams2()->whereNotNull('start_at')->pluck('exam')->intersect($earr->where('type','0')->pluck('id'))->count();
+        $ne=auth()->user()->exams2()->whereNotNull('start_at')->pluck('exam')->intersect($earr->where('type','1')->pluck('id'))->count();
+        $this->co=$nt;$this->mo=$ne;
+        $qt=0;
+        $pes=0;
+        $pga=0;
+        $uqt=array();
+        foreach (auth()->user()->exams2 as $exa) {
+            if(!empty($exa->pivot->gen) &&  in_array($exa->pivot->exam,$eall->toArray())){
+       // dd(array_keys(json_decode($exa->pivot->gen,true)));
+                $res=json_decode($exa->pivot->gen,true);
+                $arrk=array_keys($res);
+                $qt+=collect($arrk)->reduce(function (?int $carry, int|string $item) {
+                    return $carry + (is_int($item)?1:0);
+                });
+                $ca=0;
+                $rot=$rt->whereIn('id',$arrk);
+                foreach ($rot as $quest) {
+                    $bm=$quest->answers()->where('isok',true)->count()<=1;
+                    if($bm){
+                        $ab=$quest->answers()->where('isok',true)->where('answers.id',$res[$quest->id][0])->count();
+                        if($ab>0) {
+                            $ca++; $pga++;
+                        }
+                    }else{
+                        $ab2=$quest->answers()->where('isok',false)->whereIn('answers.id',$res[$quest->id])->count();
+                        if($ab2==0) {
+                            $ca++; $pga++;
+                        }
+                    }
+                }
+                if($earr->where('type','1')->where('id',$exa->pivot->exam)->count()>0) $pes+=(round(100*$ca/$earr->where('type','1')->where('id',$exa->pivot->exam)->first()->quest,2)>$ix->wperc?1:0);
+            }
+        }
+        $this->va=$qt;$this->qu=round(100*$pes/($ne>0?$ne:1),2);$this->iac=round(100*$pga/($qt>0?$qt:1),2);
+    }
+    public static function canView(): bool
+    {
+        return auth()->user()->ex>1;
+    }
+}
---
+++ resources/views/filament/widgets/us-ovv.blade.php
@@ -0,0 +1,57 @@
+<x-filament-widgets::widget class="fi-filament-info-widget">
+    <x-filament::section>
+        <div class="flex gap-x-5 gap-y-3 " style="--c-400:var(--primary-400);--c-500:var(--primary-500);--c-600:var(--primary-600);">
+            <div class="grid p-3 gap-y-2">
+                <div class="flex items-center gap-x-2">
+                    <span class="text-sm font-medium text-gray-500 dark:text-gray-400 ">
+                        Questions Answered
+                    </span>
+                </div>
+                <div
+                  class="text-3xl font-semibold tracking-tight text-custom-500 dark:text-custom-400">
+                    {{$va}}
+                </div>
+            </div>
+            <div class="grid p-2 gap-y-2">
+                <div class="flex items-center gap-x-2">
+                    <span class="text-sm font-medium text-gray-500 dark:text-gray-400">
+                       Tests Launched
+                    </span>
+                </div>
+                <div class="text-3xl font-semibold tracking-tight text-custom-500 dark:text-custom-400">
+                    {{$co}}
+                </div>
+            </div>
+            <div class="grid p-2 gap-y-2">
+                <div class="flex items-center gap-x-2">
+                    <span class="text-sm font-medium text-gray-500 dark:text-gray-400">
+                       Exams Launched
+                    </span>
+                </div>
+                <div class="text-3xl font-semibold tracking-tight text-custom-500 dark:text-custom-400">
+                    {{$mo}}
+                </div>
+            </div>
+            <div class="grid p-2 gap-y-2">
+                <div class="flex items-center gap-x-2">
+                    <span class="text-sm font-medium text-gray-500 dark:text-gray-400">
+                       Exams succeeded
+                    </span>
+                </div>
+                <div class="text-3xl font-semibold tracking-tight text-custom-500 dark:text-custom-400">
+                    {{$qu}}
+                </div>
+            </div>
+            <div class="grid p-2 gap-y-2">
+                <div class="flex items-center gap-x-2">
+                    <span class="text-sm font-medium text-gray-500 dark:text-gray-400">
+                     % Good Answers
+                    </span>
+                </div>
+                <div class="text-3xl font-semibold tracking-tight text-custom-500 dark:text-custom-400">
+                    {{$iac}}
+                </div>
+            </div>
+        </div>
+    </x-filament::section>
+</x-filament-widgets::widget>
