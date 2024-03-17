662ade919ddf4ec0f61e21fbfe989f5653e331af
mbh00 <newgoku00@gmail.com>
2024-03-17 07:14:31 +0100

    lo

--- vendor/laravel/socialite/src/Two/LinkedInProvider.php
+++ vendor/laravel/socialite/src/Two/LinkedInProvider.php
@@ -2,133 +2,133 @@
 
 namespace Laravel\Socialite\Two;
 
-use GuzzleHttp\RequestOptions;
 use Illuminate\Support\Arr;
 
 class LinkedInProvider extends AbstractProvider implements ProviderInterface
 {
-    /**
-     * The scopes being requested.
-     *
-     * @var array
-     */
-    protected $scopes = ['r_liteprofile', 'r_emailaddress'];
+/**
+* The scopes being requested.
+*
+* @var array
+*/
+protected $scopes = ['r_liteprofile', 'r_emailaddress'];
 
-    /**
-     * The separating character for the requested scopes.
-     *
-     * @var string
-     */
-    protected $scopeSeparator = ' ';
+/**
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function getAuthUrl($state)
-    {
-        return $this->buildAuthUrlFromBase('https://www.linkedin.com/oauth/v2/authorization', $state);
-    }
+The separating character for the requested scopes.
+@var string
+*/
+protected $scopeSeparator = ' ';
+/**
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function getTokenUrl()
-    {
-        return 'https://www.linkedin.com/oauth/v2/accessToken';
-    }
+{@inheritdoc}
+*/
+protected function getAuthUrl($state)
+{
+return $this->buildAuthUrlFromBase('https://www.linkedin.com/oauth/v2/authorization', $state);
+}
+/**
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function getUserByToken($token)
-    {
-        $basicProfile = $this->getBasicProfile($token);
-        $emailAddress = $this->getEmailAddress($token);
+{@inheritdoc}
+*/
+protected function getTokenUrl()
+{
+return 'https://www.linkedin.com/oauth/v2/accessToken';
+}
+/**
 
-        return array_merge($basicProfile, $emailAddress);
-    }
+Get the POST fields for the token request.
+@param string $code
+@return array
+*/
+protected function getTokenFields($code)
+{
+return parent::getTokenFields($code) + ['grant_type' => 'authorization_code'];
+}
+/**
 
-    /**
-     * Get the basic profile fields for the user.
-     *
-     * @param  string  $token
-     * @return array
-     */
-    protected function getBasicProfile($token)
-    {
-        $fields = ['id', 'firstName', 'lastName', 'profilePicture(displayImage~:playableStreams)'];
+{@inheritdoc}
+*/
+protected function getUserByToken($token)
+{
+$basicProfile = $this->getBasicProfile($token);
+$emailAddress = $this->getEmailAddress($token);
 
-        if (in_array('r_liteprofile', $this->getScopes())) {
-            array_push($fields, 'vanityName');
-        }
+return array_merge($basicProfile, $emailAddress);
+}
 
-        $response = $this->getHttpClient()->get('https://api.linkedin.com/v2/me', [
-            RequestOptions::HEADERS => [
-                'Authorization' => 'Bearer '.$token,
-                'X-RestLi-Protocol-Version' => '2.0.0',
-            ],
-            RequestOptions::QUERY => [
-                'projection' => '('.implode(',', $fields).')',
-            ],
-        ]);
+/**
 
-        return (array) json_decode($response->getBody(), true);
-    }
+Get the basic profile fields for the user.
 
-    /**
-     * Get the email address for the user.
-     *
-     * @param  string  $token
-     * @return array
-     */
-    protected function getEmailAddress($token)
-    {
-        $response = $this->getHttpClient()->get('https://api.linkedin.com/v2/emailAddress', [
-            RequestOptions::HEADERS => [
-                'Authorization' => 'Bearer '.$token,
-                'X-RestLi-Protocol-Version' => '2.0.0',
-            ],
-            RequestOptions::QUERY => [
-                'q' => 'members',
-                'projection' => '(elements*(handle~))',
-            ],
-        ]);
+@param string $token
 
-        return (array) Arr::get((array) json_decode($response->getBody(), true), 'elements.0.handle~');
-    }
+@return array
+*/
+protected function getBasicProfile($token)
+{
+$url = 'https://api.linkedin.com/v2/me?projection=(id,firstName,lastName,profilePicture(displayImage~:playableStreams))';
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function mapUserToObject(array $user)
-    {
-        $preferredLocale = Arr::get($user, 'firstName.preferredLocale.language').'_'.Arr::get($user, 'firstName.preferredLocale.country');
-        $firstName = Arr::get($user, 'firstName.localized.'.$preferredLocale);
-        $lastName = Arr::get($user, 'lastName.localized.'.$preferredLocale);
+$response = $this->getHttpClient()->get($url, [
+'headers' => [
+'Authorization' => 'Bearer '.$token,
+'X-RestLi-Protocol-Version' => '2.0.0',
+],
+]);
 
-        $images = (array) Arr::get($user, 'profilePicture.displayImage~.elements', []);
-        $avatar = Arr::first($images, function ($image) {
-            return (
-                $image['data']['com.linkedin.digitalmedia.mediaartifact.StillImage']['storageSize']['width'] ??
-                $image['data']['com.linkedin.digitalmedia.mediaartifact.StillImage']['displaySize']['width']
-            ) === 100;
-        });
-        $originalAvatar = Arr::first($images, function ($image) {
-            return (
-                $image['data']['com.linkedin.digitalmedia.mediaartifact.StillImage']['storageSize']['width'] ??
-                $image['data']['com.linkedin.digitalmedia.mediaartifact.StillImage']['displaySize']['width']
-            ) === 800;
-        });
+return (array) json_decode($response->getBody(), true);
+}
 
-        return (new User)->setRaw($user)->map([
-            'id' => $user['id'],
-            'nickname' => null,
-            'name' => $firstName.' '.$lastName,
-            'first_name' => $firstName,
-            'last_name' => $lastName,
-            'email' => Arr::get($user, 'emailAddress'),
-            'avatar' => Arr::get($avatar, 'identifiers.0.identifier'),
-            'avatar_original' => Arr::get($originalAvatar, 'identifiers.0.identifier'),
-        ]);
-    }
+/**
+
+Get the email address for the user.
+
+@param string $token
+
+@return array
+*/
+protected function getEmailAddress($token)
+{
+$url = 'https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))';
+
+$response = $this->getHttpClient()->get($url, [
+'headers' => [
+'Authorization' => 'Bearer '.$token,
+'X-RestLi-Protocol-Version' => '2.0.0',
+],
+]);
+
+return (array) Arr::get((array) json_decode($response->getBody(), true), 'elements.0.handle~');
+}
+
+/**
+
+{@inheritdoc}
+*/
+protected function mapUserToObject(array $user)
+{
+$preferredLocale = Arr::get($user, 'firstName.preferredLocale.language').'_'.Arr::get($user, 'firstName.preferredLocale.country');
+$firstName = Arr::get($user, 'firstName.localized.'.$preferredLocale);
+$lastName = Arr::get($user, 'lastName.localized.'.$preferredLocale);
+
+$images = (array) Arr::get($user, 'profilePicture.displayImage~.elements', []);
+$avatar = Arr::first(Arr::where($images, function ($image) {
+return $image['data']['com.linkedin.digitalmedia.mediaartifact.StillImage']['storageSize']['width'] === 100;
+}));
+$originalAvatar = Arr::first(Arr::where($images, function ($image) {
+return $image['data']['com.linkedin.digitalmedia.mediaartifact.StillImage']['storageSize']['width'] === 800;
+}));
+
+return (new User)->setRaw($user)->map([
+'id' => $user['id'],
+'nickname' => null,
+'name' => $firstName.' '.$lastName,
+'first_name' => $firstName,
+'last_name' => $lastName,
+'email' => Arr::get($user, 'emailAddress'),
+'avatar' => Arr::get($avatar, 'identifiers.0.identifier'),
+'avatar_original' => Arr::get($originalAvatar, 'identifiers.0.identifier'),
+]);
+}
+
 }
